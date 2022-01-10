e648087b9b4cb77e928f63b87908e9bf6bf6afcd
kagaju <kagajugrace@gmail.com>
2022-01-10 12:11:17 +0200

    datachecker, and add datacheker in userrouter, deleteone and getone payment

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userController from "../controllers/userController";
 
+import DataChecker from "../middlewares/datachecker";
+
 const userRouter=express.Router();
 
-userRouter.post( "/register", userController.createUser);
+userRouter.post( "/register", DataChecker.isEmailExist, DataChecker.isPhoneExist, userController.createUser);
 userRouter.get("/all", userController.getAllUsers);
 userRouter.get("/:id",userController.getOneUser);
 userRouter.delete("/:id",userController.deleteOneUser);
